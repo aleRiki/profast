e695d9eabe13942b41443f16fde7986a4cd6e82c
ricardodeveloper <128541735+aleRiki@users.noreply.github.com>
2025-08-24 13:00:17 -0400

    comit

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ from router import address
 from router import organization
 from router import auth
 from router import server
+from fastapi.middleware.cors import CORSMiddleware
 # Crear tablas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "https://your-frontend-domain.com",
+    # Agrega aquí más dominios permitidos
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluir routers
 app.include_router(user.router)
